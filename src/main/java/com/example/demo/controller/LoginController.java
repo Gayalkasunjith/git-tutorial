410c68b94d4f33a42f4381f26c5c792f92f8dc51
Gayal <gkasunjith@gmail.com>
2018-08-11 20:13:53 +0530

    create login page

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,9 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@RequestMapping(value = "/")
 public class LoginController {
 
+    @GetMapping(value = "login")
+    public ModelAndView getLogin() {
+        return new ModelAndView("login");
+    }
 
 }
